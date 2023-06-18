773d073c5f645f6f2a6fa3da06064c65d3aabcdc
nguyenvx007 <130591557+nguyenvx007@users.noreply.github.com>
2023-06-18 09:10:07 -0600

    Create csv-split-commas-in-double-quotes.py

---
+++ Python Functions Files Dictionaries - Course 2/csv-split-commas-in-double-quotes.py
@@ -0,0 +1,12 @@
+
+### Splitting/Separating CSV Ignoring Commas in Double Quotes ####
+# 1. Import csv module
+import csv
+# Open csv file with" csv.reader()" funtion
+fileref = csv.reader("test2.csv")
+# Each value using "next()" function is strip of leading and trailing space using list comprehension and handles commas with double 
+row = next(fileref)
+for row in lines:
+    row=row.split(",")
+    print(row)
+

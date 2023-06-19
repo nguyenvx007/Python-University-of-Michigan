e16eac49a2ebec15cde35104007ff4f5e287dc85
nguyenvx007 <130591557+nguyenvx007@users.noreply.github.com>
2023-06-19 01:04:54 -0600

    Add

--- Python Functions Files Dictionaries - Course 2/csv-split-commas-in-double-quotes.py
+++ Python Functions Files Dictionaries - Course 2/csv-split-commas-in-double-quotes.py
@@ -4,7 +4,7 @@
 import csv
 # Open csv file with" csv.reader()" funtion
 fileref = csv.reader("test2.csv")
-# Each value using "next()" function is strip of leading and trailing space using list comprehension and handles commas with double 
+# "next()" function reads first row as list of values. Each value is then strip of leading and trailing space using list comprehension and handles commas with double 
 row = next(fileref)
 for row in lines:
     row=row.split(",")

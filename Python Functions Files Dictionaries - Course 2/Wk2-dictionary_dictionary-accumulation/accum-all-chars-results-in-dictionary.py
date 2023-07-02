37161f9b240bfb4ba67adfc758e9ca2b0c6a2ff9
nguyenvx007 <130591557+nguyenvx007@users.noreply.github.com>
2023-07-02 16:18:21 -0600

    Create accum-all-chars-results-in-dictionary.py

---
+++ Python Functions Files Dictionaries - Course 2/Wk2-dictionary_dictionary-accumulation/accum-all-chars-results-in-dictionary.py
@@ -0,0 +1,10 @@
+# Accumulating Multiple Results in Dictionary
+
+fileref = open("test.txt","r")
+chars = fileref.read()
+dict = {}
+for char in chars:
+    if char != dict[char]
+        dict[char] = 0
+dict[char] = dict[char] + 1
+print(dict.items())
